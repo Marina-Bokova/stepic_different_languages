add6dcf42d97c70a8b3ef65c09d08869de025053
Marina <mary.v.bokova@gmail.com>
2022-10-25 19:13:56 +0700

    Initial commit

---
+++ test_items.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+import time
+
+
+def test_different_interface_languages(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    # time.sleep(30)
+    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket") is not None
