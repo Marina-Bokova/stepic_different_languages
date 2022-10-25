db80021590612982c4a266b5bd10d438c83691e6
Marina <mary.v.bokova@gmail.com>
2022-10-25 19:38:31 +0700

    Rename test method

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 import time
 
 
-def test_different_interface_languages(browser):
+def test_find_card_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     # time.sleep(30)
